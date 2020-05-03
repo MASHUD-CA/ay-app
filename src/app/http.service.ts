d26f4c843243ccd63a8342daf150c3737dfecb64
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-05-03 17:34:16 -0400

    Updated http.service.ts

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
+//import
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  constructor() {}
-
+  constructor(private http: HttpClient) {}
+  /*
   myMethod() {
     return console.log('hey what is up');
+  }
+*/
+
+  getBeer() {
+    return this.http.get('https://api.openbrewerydb.org/breweries');
   }
 }

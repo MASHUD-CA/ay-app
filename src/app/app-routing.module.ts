6aeb9a06b6152cf607cb6de061f5e8b4ac6c5088
Mashud Ahmed <mashudahmed1980@gmail.com>
2020-05-03 15:05:01 -0400

    Added div at home.component.html to check functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+// Imported new Item
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'list', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
